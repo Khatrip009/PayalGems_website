b211dbac6e3c8a642de87009f48f8f45fbdaca8c
Khatrip009 <Khatrip.009@gmail.com>
2026-06-23 15:20:42 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-self.addEventListener("push", event => {
-  let data = {};
-  try { data = event.data.json(); } catch {}
+self.addEventListener('push', function(event) {
+  const data = event.data?.json() || { title: 'Notification', body: '' };
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/logo_minalgems.png',
+    data: { url: data.data?.url || '/' }
+  });
+});
 
-  event.waitUntil(
-    self.registration.showNotification(data.title || "Update", {
-      body: data.body || "",
-      icon: "/logo.png",
-      data,
-    })
-  );
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
 });
